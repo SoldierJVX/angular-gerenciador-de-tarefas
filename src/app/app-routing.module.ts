6b573d21c2244aee00983ea117723abfd348ee69
SoldierJVX <jv_rss@hotmail.com>
2020-09-16 22:51:25 -0300

    add listar tarefa and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TarefaRoutes } from './tarefas';
 
 export const routes: Routes = [
-    
+    {
+        path: '',
+        redirectTo: '/tarefas/listar',
+        pathMatch: 'full'
+    },
+    ...TarefaRoutes
 ]
 
 @NgModule({
