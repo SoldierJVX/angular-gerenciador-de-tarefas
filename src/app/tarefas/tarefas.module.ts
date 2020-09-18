fdddc19c1696814ba1567763b9ad9a129c4a0c8a
SoldierJVX <jv_rss@hotmail.com>
2020-09-17 22:38:37 -0300

    implementing task listing

--- src/app/tarefas/tarefas.module.ts
+++ src/app/tarefas/tarefas.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 import { TarefaService } from './shared';
 import { ListarTarefaComponent } from './listar';
-
-
 
 @NgModule({
   declarations: [
     ListarTarefaComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule,
+    FormsModule
   ],
   providers: [
     TarefaService
